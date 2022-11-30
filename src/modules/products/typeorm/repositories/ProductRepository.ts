edc5884bf4922fd92f633fb472bc4078eb920d69
debtil <matheus.debtilsouza@gmail.com>
2022-11-30 14:31:15 -0300

    configurando ambiente

---
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -0,0 +1,11 @@
+import { Repository } from "typeorm";
+import Product from "../entities/Product";
+
+export default class ProductRepository extends Repository<Product>{
+    public async findByName(name: string): Promise<Product | undefined>{
+        const product = await this.findOne({
+            where: {name}
+        })
+        return product;
+    }
+}
