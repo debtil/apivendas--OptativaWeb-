9910e1567cb08d66206773a0c491db9b1980dd2f
debtil <matheus.debtilsouza@gmail.com>
2022-12-14 13:27:53 -0300

    celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,18 @@
 import 'reflect-metadata';// esse import precisa sempre ser na primeira linha
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/appError';
 import 'src/shared/typeorm';
+import {errors} from 'celebrate';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 app.use((error: Error, request: Request, response: Response, next: NextFunction) =>{
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
